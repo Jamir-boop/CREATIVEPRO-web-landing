888dfb332bdcc24204d39fe131008feddc9ebf9e
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-08-25 14:22:21 -0500

    back html

--- assets/js/barra_navegacion.js
+++ assets/js/barra_navegacion.js
@@ -3,7 +3,7 @@ const links = document.querySelectorAll('.scrollto');
 links.forEach((item) => {
     item.addEventListener('click', function (){
         if (window.location.href.indexOf("portfolio") !== -1){
-            window.location.href = "index.php?to="+item.getAttribute("data-link");
+            window.location.href = "index.html?to="+item.getAttribute("data-link");
 
         }else{
             const adonde = document.getElementById(item.getAttribute("data-link"));
