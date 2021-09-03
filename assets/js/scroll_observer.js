65b90935384ae9cf7e0fd2b876c0d6f70b12d58a
Jamir Vargas <73477811+Jamir-boop@users.noreply.github.com>
2021-09-03 00:34:13 -0500

    mejor boton navegacion

--- assets/js/scroll_observer.js
+++ assets/js/scroll_observer.js
@@ -1,11 +1,17 @@
 window.addEventListener("scroll", () => {
     const scrolled = window.scrollY;
 
-    if (scrolled > 1000){
+    if (scrolled >= 1000 && scrolled <= 10000){
         document.querySelector(".scroll_top").style.visibility = "visible";
-
     }else{
-        document.querySelector(".scroll_top").style.visibility = "hidden";
+        document.querySelector(".scroll_top").style.visibility = "hidden";;
+    }
+
+
+    if (scrolled >= 10000){
+        document.querySelector(".scroll_comprar").style.visibility = "visible";
+    }else{
+        document.querySelector(".scroll_comprar").style.visibility = "hidden";
     }
 
 });
